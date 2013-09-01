2026ffc168edbf9d27365a63d0a165c73e5d539d
Michael Angstadt <mike.angstadt@gmail.com>
2013-09-01 19:14:53 +0000

    "EnumParameterValue" doesn't need "equals()" or "hashCode()" because child classes are all treated as case classes.

--- src/main/java/biweekly/parameter/EnumParameterValue.java
+++ src/main/java/biweekly/parameter/EnumParameterValue.java
@@ -52,31 +52,6 @@ public class EnumParameterValue {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EnumParameterValue other = (EnumParameterValue) obj;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return value;
 	}
