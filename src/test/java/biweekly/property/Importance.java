a06f3ee2eafcab0add8a7df3fd744ceffe88b7d4
Michael Angstadt <mike.angstadt@gmail.com>
2013-09-21 22:08:51 +0000

    Added the example classes that are used in the Wiki.

---
+++ src/test/java/biweekly/property/Importance.java
@@ -0,0 +1,77 @@
+package biweekly.property;
+
+import java.util.List;
+
+import biweekly.component.ICalComponent;
+
+/*
+ Copyright (c) 2013, Michael Angstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Example property class used on the Wiki.
+ * @author Michael Angstadt
+ */
+public class Importance extends ICalProperty {
+	private Integer number;
+	private String text;
+
+	public Importance(Integer value) {
+		this.number = value;
+	}
+
+	public Importance(String text) {
+		this.text = text;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+		this.text = null;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.number = null;
+		this.text = text;
+	}
+
+	//optional
+	@Override
+	protected void validate(List<ICalComponent> parentComponents, List<String> warnings) {
+		if (number == null && text == null) {
+			warnings.add("Value is null.");
+		}
+
+		if (number != null && number < 0) {
+			warnings.add("Number cannot be less than 0");
+		}
+	}
+}
---
+++ src/test/java/biweekly/property/marshaller/ImportanceMarshaller.java
@@ -0,0 +1,163 @@
+package biweekly.property.marshaller;
+
+import java.util.List;
+
+import biweekly.ICalDataType;
+import biweekly.io.CannotParseException;
+import biweekly.io.SkipMeException;
+import biweekly.io.json.JCalValue;
+import biweekly.io.xml.XCalElement;
+import biweekly.parameter.ICalParameters;
+import biweekly.property.Importance;
+
+/*
+ Copyright (c) 2013, Michael Angstadt
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met: 
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer. 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution. 
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * Example marshaller class used on the Wiki.
+ * @author Michael Angstadt
+ */
+public class ImportanceMarshaller extends ICalPropertyMarshaller<Importance> {
+	public ImportanceMarshaller() {
+		super(Importance.class, "X-IMPORTANCE", ICalDataType.INTEGER);
+	}
+
+	//optional
+	//determines the iCal data type of the property's value
+	@Override
+	protected ICalDataType _dataType(Importance property) {
+		if (property.getText() != null) {
+			return ICalDataType.TEXT;
+		}
+		return ICalDataType.INTEGER;
+	}
+
+	//optional
+	//tweaks the parameters before the property is written
+	@Override
+	protected void _prepareParameters(Importance property, ICalParameters copy) {
+		Integer value = property.getNumber();
+		if (value != null && value >= 10) {
+			copy.put("X-MESSAGE", "very important!!");
+		}
+	}
+
+	//required
+	//writes the property to a plain-text iCal
+	@Override
+	protected String _writeText(Importance property) {
+		return write(property);
+	}
+
+	//required
+	//parses the property from a plain-text iCal
+	@Override
+	protected Importance _parseText(String value, ICalDataType dataType, ICalParameters parameters, List<String> warnings) {
+		value = unescape(value);
+		return parse(value, dataType);
+	}
+
+	//optional
+	//writes the property to an XML document (xCal)
+	@Override
+	protected void _writeXml(Importance property, XCalElement element) {
+		Integer value = property.getNumber();
+		if (value != null) {
+			if (value > 100) {
+				throw new SkipMeException("Way too high.");
+			}
+			element.append(ICalDataType.INTEGER, value.toString()); //writes: <x-importance><integer>1</integer></x-importance>
+			return;
+		}
+
+		String text = property.getText();
+		if (text != null) {
+			element.append(ICalDataType.TEXT, text); //writes: <x-importance><text>high</text></x-importance>
+		}
+
+	}
+
+	//optional
+	//reads the property from an XML document (xCal)
+	@Override
+	protected Importance _parseXml(XCalElement element, ICalParameters parameters, List<String> warnings) {
+		String text = element.first(ICalDataType.TEXT);
+		if (text != null) {
+			return new Importance(text);
+		}
+
+		String number = element.first(ICalDataType.INTEGER);
+		if (number != null) {
+			try {
+				return new Importance(Integer.valueOf(number));
+			} catch (NumberFormatException e) {
+				throw new CannotParseException("Numeric value expected: " + number);
+			}
+		}
+
+		return new Importance(0);
+	}
+
+	//optional
+	//writes the property to a JSON document (jCal)
+	@Override
+	protected JCalValue _writeJson(Importance property) {
+		return JCalValue.single(write(property));
+	}
+
+	//optional
+	//reads the property from a JSON document (jCal)
+	@Override
+	protected Importance _parseJson(JCalValue value, ICalDataType dataType, ICalParameters parameters, List<String> warnings) {
+		String valueStr = value.asSingle();
+		return parse(valueStr, dataType);
+	}
+
+	private Importance parse(String value, ICalDataType dataType) {
+		if (dataType == ICalDataType.TEXT) {
+			return new Importance(value);
+		}
+
+		try {
+			return new Importance(Integer.valueOf(value));
+		} catch (NumberFormatException e) {
+			throw new CannotParseException("Numeric value expected: " + value);
+		}
+	}
+
+	private String write(Importance property) {
+		String text = property.getText();
+		if (text != null) {
+			return text;
+		}
+
+		Integer number = property.getNumber();
+		if (number != null) {
+			return number.toString();
+		}
+
+		return "";
+	}
+}
